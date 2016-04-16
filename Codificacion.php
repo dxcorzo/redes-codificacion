74aee2433f7a9a0c3634f8fdbdd5236b9efdfcdc
dxcorzo <dxcorzo@gmail.com>
2016-04-15 19:53:31 -0500

    Desarrollo total b8zs
    Se encuentran hechos:
    
    * ami
    * b8zs con logica positiva y negativa
    
    cada uno de los métodos va con su respectiva prueba

--- Codificacion.php
+++ Codificacion.php
@@ -1,11 +1,6 @@
 <?php
-        
-//Vector inicial, de ejemplo tomamos lo que vimos en clase
-$vectorBits = [0,1,0,0,1,1,1,0]; //ejercicio para ami
 
-$vectorBits_b8zs = [1,1,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0]; //ejercicio b8zs
-
-function b8zs($vector)
+function b8zs($vector, $logicaPositiva)
 {
 	$ret = []; //retorno
 	
@@ -18,57 +13,91 @@ function b8zs($vector)
 	//Vector de reemplazo cuando el bit anterior es negativo
 	$secuenciaNegativa = [0,0,0,-1,1,0,1,-1];
 	
-	$vectorFiltro = BuscarEnVector($secuenciaBuscar, $vector);
+	//dividir el vector segun la secuencia de 8 ceros junto a sus sobrantes
+	$vectorCoincidencias = DividirVector($secuenciaBuscar, $vector);
+
+	//determina los cambios de señal hechos por ami
+	$cambioSenal = !$logicaPositiva;
 	
-	print_r($vectorFiltro);
-	
-	foreach($vectorFiltro as $i => $valor)
+	foreach($vectorCoincidencias as $vectorOperar)
 	{
-		$minBound = $valor[0];
-		$maxBound = $valor[count($valor) - 1];
+		//indice inicial para hacer el slice del vector original
+		$min = $vectorOperar[0];
 		
-		echo "Min: $minBound / Max: $maxBound\n";
+		//total de elementos a tomar para hacer el slice
+		$conteo = count($vectorOperar);
+		
+		//basado en los limites detectados, hacer el slice
+		$vectorDividido = array_slice($vector, $min, $conteo);
+		
+		//determinar cual es el metodo que aplica para resolver la secuencia
+		$metodoResolucion = DetectarMetodoResolucion($vectorDividido, "b8zs");
+		
+		switch($metodoResolucion)
+		{
+			case "ami":
+				$ret[] = ami($vectorDividido, $cambioSenal);
+				break;
+			case "b8zs":
+				//hacer los reemplazos de la secuencia dependiendo del signo anterior
+				$ret[] = $cambioSenal == true ? $secuenciaPositiva : $secuenciaNegativa;
+				break;
+		}
+	}
+	
+	//como el resultado es un vector, usamos el operador ... para concatenar todos los vectores internos
+	return array_merge(...$ret);
+}
+
+//determinar cual el es metodo de resolucion que aplica para el vector en cuestion
+function DetectarMetodoResolucion($vectorRev, $metodoVerificar)
+{
+	//buscar los elementos unicos del vector, si el conteo de ese vector es 1 quiere decir
+	//que solo tiene 1 elemento igual en todas sus posiciones, adicional si preguntamos que
+	//elemento hay en la posicion [0] determinamos que metodo usar
+	return ((count(array_unique($vectorRev)) == 1) && $vectorRev[0] == 0) ? $metodoVerificar : "ami";
+}
+
+//Divide un vector basado en otro vector, incluyendo sus sobrantes.
+//este método sirve para implementacion tanto en dbz3 como b8zs
+function DividirVector($secuencia, $vectorBuscar)
+{
+	$total = count($secuencia);
+	$atotal = count($vectorBuscar);
+
+	$i = 0;
+	$k = 0;
+
+	$ret = [];
+  
+	while($i < $atotal)
+	{
+		if(!isset($ret[$k])) $ret[$k] = [];
+
+		if ($secuencia == array_slice($vectorBuscar,$i,$total))
+		{
+			$k++;
+
+			for($o = 0; $o < $total; $o++)
+			{
+				$ret[$k][] = $i;
+				$i++;
+			}
+
+			$k++;
+		}
+		else
+		{
+			$ret[$k][]  = $i;    
+			$i++;
+		}
 	}
 	
 	return $ret;
 }
 
-//Busca dentro de un vector otro vector para determinar sus indices
-//la idea es hacer un split pero en vez de buscar un caracter en especifico
-//se usa otro vector para hacer el split
-function BuscarEnVector($vectorBuscar, $vector) 
-{
-    $listaIndices = array_keys($vector, $vectorBuscar[0]);
-    $ret = [];
-	
-    foreach ($listaIndices as $indice) 
-	{
-        $adicionar = true;
-        $resultado = [];
-        
-		foreach ($vectorBuscar as $i => $valor) 
-		{
-            if (!(isset($vector[$indice + $i]) && $vector[$indice + $i] == $valor)) 
-			{
-                $adicionar = false;
-                break;
-            }
-			
-            $resultado[] = $indice + $i;
-        }
-		
-        if ($adicionar == true) 
-		{ 
-            $ret[] = $resultado;
-        }
-    }
-	
-    return $ret;
-}
 
-
-
-function dbz3($vector)
+function hdb3($vector)
 {
 	$ret = [];
 	
@@ -84,11 +113,15 @@ function ami($vector, &$signalChange)
     $bitAux = null;
     $ret = [];
     
+	//recorrer todo el vector
     foreach($vector as $bit)
     {
         if($bit == 1)
         {
+			//colocar 1 o -1 dependiendo del cambio de señal
             $bitAux = $signalChange ? -1 : 1;
+			
+			//cambiar señal
             $signalChange = !$signalChange;
         }
         else
@@ -96,6 +129,7 @@ function ami($vector, &$signalChange)
             $bitAux = 0;
         }
         
+		//adicionar item al vector
         array_push($ret, $bitAux);
     }
     
@@ -117,14 +151,17 @@ function RenderVector($vector)
     return "<table cellspacing='0'><tr class='head'>$header</tr><tr class='ld'>$s1</tr><tr class='lu'>$s2</tr></table>";
 }
 
-/*Codigo para probar AMI*/
+/*Codigo para probar ami*/
+//$vectorBits = [0,1,0,0,1,1,1,0];
 //$signalChange = false;
-//$vec = ami($vectorBits, $signalChange);
-//print_r(ami($vectorBits, $signalChange)); //este dump sirve para depurar el metodo de codificacion seleccionado
-
-//codigo para probar b8zs
-$vec = b8zs($vectorBits_b8zs);
-
-
-//pintar el vector codificado
 //echo RenderVector(ami($vectorBits));
+
+
+/*Codigo para probar b8zs*/
+//$vectorBits = [1,0,0,0,0,0,0,0,0,1,1];
+//echo RenderVector(b8zs($vectorBits, true)); //logica positiva
+
+//$vectorBits = [1,1,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0];
+//echo RenderVector(b8zs($vectorBits, false)); //logica negativa
+
+/*codigo para probar dbz3*/
