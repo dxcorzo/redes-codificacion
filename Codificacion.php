d6666518a44e8209a22c55c8759938f20b637359
dxcorzo <dxcorzo@gmail.com>
2016-04-29 12:03:37 -0500

    Inclusión hdb3
    hdb3 implementado

--- Codificacion.php
+++ Codificacion.php
@@ -18,6 +18,7 @@ function b8zs($vector, $logicaPositiva)
 
 	//determina los cambios de señal hechos por ami
 	$cambioSenal = !$logicaPositiva;
+	$totalUnosTransmitidos = 0;
 	
 	foreach($vectorCoincidencias as $vectorOperar)
 	{
@@ -27,7 +28,7 @@ function b8zs($vector, $logicaPositiva)
 		switch($metodoResolucion)
 		{
 			case "ami":
-				$ret[] = ami($vectorOperar, $cambioSenal);
+				$ret[] = ami($vectorOperar, $cambioSenal, $totalUnosTransmitidos);
 				break;
 			case "b8zs":
 				//hacer los reemplazos de la secuencia dependiendo del signo anterior
@@ -40,13 +41,99 @@ function b8zs($vector, $logicaPositiva)
 	return array_merge(...$ret);
 }
 
+function hdb3($vector)
+{
+	$ret = []; //retorno
+	
+	//vector de busqueda
+	$secuenciaBuscar = [0,0,0,0];
+	
+	//vectores de reemplazo (reglas)
+	$secuenciaPositiva_Par = [-1,0,0,-1];
+	$secuenciaNegativa_Par = [1,0,0,1];
+	$secuenciaPositiva_Impar = [0,0,0,1];
+	$secuenciaNegativa_Impar = [0,0,0,-1];
+	
+	//dividir el vector segun la secuencia de 4 ceros junto a sus sobrantes
+	$vectorCoincidencias = DividirVector($secuenciaBuscar, $vector);
+
+	//determina los cambios de señal hechos por ami
+	$cambioSenal = false;
+	$totalUnosTransmitidos = 0;
+	
+	foreach($vectorCoincidencias as $vectorOperar)
+	{
+		//determinar cual es el metodo que aplica para resolver la secuencia
+		$metodoResolucion = DetectarMetodoResolucion($vectorOperar, "hdb3");
+		
+		switch($metodoResolucion)
+		{
+			case "ami":
+				$ret[] = ami($vectorOperar, $cambioSenal, $totalUnosTransmitidos);
+				$cambioSenal = !$cambioSenal;
+				break;
+			case "hdb3":
+				if($totalUnosTransmitidos % 2 == 0) //Si es par
+				{
+					$ret[] = $cambioSenal ? $secuenciaNegativa_Par : $secuenciaPositiva_Par;
+					$totalUnosTransmitidos = $totalUnosTransmitidos + 2;
+				}
+				else //es impar
+				{
+					$ret[] = $cambioSenal ? $secuenciaNegativa_Impar : $secuenciaPositiva_Impar;
+					$totalUnosTransmitidos++;
+				}
+				
+				break;
+		}
+	}
+	
+	//como el resultado es un vector, usamos el operador ... para concatenar todos los vectores internos
+	return array_merge(...$ret);
+}
+
+//$vector es el vector de bits
+//&$signalChange es un bool pasado por referencia que indica si inicio con cambio de señal
+function ami($vector, &$signalChange, &$totalUnosTransmitidos)
+{
+    $bitAux = null;
+    $ret = [];
+    
+	//recorrer todo el vector
+    foreach($vector as $bit)
+    {
+        if($bit == 1)
+        {
+			//colocar 1 o -1 dependiendo del cambio de señal
+            $bitAux = $signalChange ? -1 : 1;
+			
+			//cambiar señal
+            $signalChange = !$signalChange;
+			
+			//sumar el # de 1 transmitidos independiente si es positivo o negativo
+			//luego lo usaremos para determinar si es par o impar
+			$totalUnosTransmitidos++;
+        }
+        else
+        {
+            $bitAux = 0;
+        }
+        
+		//adicionar item al vector
+        array_push($ret, $bitAux);
+    }
+    
+    return $ret;
+}
+
+
 //determinar cual el es metodo de resolucion que aplica para el vector en cuestion
 function DetectarMetodoResolucion($vectorRev, $metodoVerificar)
 {
 	//buscar los elementos unicos del vector, si el conteo de ese vector es 1 quiere decir
 	//que solo tiene 1 elemento igual en todas sus posiciones, adicional si preguntamos que
 	//elemento hay en la posicion [0] determinamos que metodo usar
-	return ((count(array_unique($vectorRev)) == 1) && $vectorRev[0] == 0) ? $metodoVerificar : "ami";
+	return ((count($vectorRev) == 4 || count($vectorRev) == 8) && (count(array_unique($vectorRev)) == 1) && $vectorRev[0] == 0) ? $metodoVerificar : "ami";
 }
 
 //Divide un vector basado en otro vector, incluyendo sus sobrantes.
@@ -73,56 +160,17 @@ function DividirVector($patron, $vectorBuscar)
     return $vectorBuscar2;
 }
 
-
-function hdb3($vector)
-{
-	$ret = [];
-	
-	//...
-	
-	return $ret;
-}
-
-//$vector es el vector de bits
-//&$signalChange es un bool pasado por referencia que indica si inicio con cambio de señal
-function ami($vector, &$signalChange)
-{
-    $bitAux = null;
-    $ret = [];
-    
-	//recorrer todo el vector
-    foreach($vector as $bit)
-    {
-        if($bit == 1)
-        {
-			//colocar 1 o -1 dependiendo del cambio de señal
-            $bitAux = $signalChange ? -1 : 1;
-			
-			//cambiar señal
-            $signalChange = !$signalChange;
-        }
-        else
-        {
-            $bitAux = 0;
-        }
-        
-		//adicionar item al vector
-        array_push($ret, $bitAux);
-    }
-    
-    return $ret;
-}
-//PILAS! no modifiquen este algoritmo, funciona para cualquier tipo de codificacion
-function RenderVector($vector)
+function RenderVector($base, $vector)
 {
     $s1 = "";
     $s2 = "";
     $header = "";
-    foreach($vector as $bit)
+	
+    foreach($vector as $i => $bit)
     {
-        $header .= "<th>$bit</th>";
-        $s1 .= "<td ".(($bit == 1) ? "class='up'" : "").">-</td>";
-        $s2 .= "<td ".(($bit == -1) ? "class='dw'" : "").">-</td>";
+        $header .= "<th>".$base[$i]."</th>"; //mostrar el bit con el que se calculo el resultado		
+        $s1 .= "<td ".(($bit == 1) ? "class='up'" : "").">-</td>"; //linea para los positivos
+        $s2 .= "<td ".(($bit == -1) ? "class='dw'" : "").">-</td>"; //linea para los negativos
     }
     
     return "<table cellspacing='0'><tr class='head'>$header</tr><tr class='ld'>$s1</tr><tr class='lu'>$s2</tr></table>";
@@ -131,14 +179,17 @@ function RenderVector($vector)
 /*Codigo para probar ami*/
 //$vectorBits = [0,1,0,0,1,1,1,0];
 //$signalChange = false;
-//echo RenderVector(ami($vectorBits));
+//echo RenderVector($vectorBits, ami($vectorBits));
 
 
 /*Codigo para probar b8zs*/
 //$vectorBits = [1,0,0,0,0,0,0,0,0,1,1];
-//echo RenderVector(b8zs($vectorBits, true)); //logica positiva
+//echo RenderVector($vectorBits, b8zs($vectorBits, true)); //logica positiva
 
 //$vectorBits = [1,1,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0];
-//echo RenderVector(b8zs($vectorBits, false)); //logica negativa
+//echo RenderVector($vectorBits, b8zs($vectorBits, false)); //logica negativa
 
 /*codigo para probar hdb3*/
+//$vectorBits = [1,0,0,0,0,0,0,0,0,0,0,1,0,0]; //funciona ok
+//$vectorBits = [1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0]; //ok
+//echo RenderVector($vectorBits, hdb3($vectorBits));
